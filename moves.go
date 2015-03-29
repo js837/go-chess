971bc6168452ef424a4896e8edc0199191e5774f
Jake <stacey.jake@gmail.com>
2015-03-29 00:34:31 +0000

    Bishop, Queen, Rook, Pawns working

--- moves.go
+++ moves.go
@@ -9,13 +9,17 @@ type Move struct {
 func (p *Position) GetMoves() []Move {
 	var moves = []Move{}
 	for from, piece := range p.board {
-		switch piece.Type() {
-		case Queen:
-			moves = append(moves, QueenMoves(p, from)...)
-		case Rook:
-			moves = append(moves, RookMoves(p, from)...)
-		case Bishop:
-			moves = append(moves, BishopMoves(p, from)...)
+		if piece.Color() == White {
+			switch piece.Type() {
+			case Pawn:
+				moves = append(moves, PawnMoves(p, from)...)
+			case Queen:
+				moves = append(moves, QueenMoves(p, from)...)
+			case Rook:
+				moves = append(moves, RookMoves(p, from)...)
+			case Bishop:
+				moves = append(moves, BishopMoves(p, from)...)
+			}
 		}
 
 	}
@@ -93,5 +97,52 @@ func BishopMoves(p *Position, from int) []Move {
 			}
 		}
 	}
+	return moves
+}
+
+func PawnMoves(p *Position, from int) []Move {
+	// Get the possible moves o
+	// TODO: Get the colour from the position
+	myColour := p.board[from].Color()
+
+	rank, _ := GetRankFile(from)
+
+	var moves = []Move{}
+	var localFirstRank, localPromoRank, localN, localE, localW int
+
+	if myColour == White {
+		localFirstRank, localPromoRank = 1, 6
+		localN, localE, localW = N, E, W
+	} else {
+		localFirstRank, localPromoRank = 6, 1
+		localN, localE, localW = -N, -E, -W
+	}
+
+	// Promotion
+	if to := from + localN; rank == localPromoRank && p.board[to] == NoPiece {
+		moves = append(moves, Move{from, to, Queen}) // Only queen for now.
+	} else {
+		// Single move
+		if to := from + localN; p.board[to] == NoPiece {
+			moves = append(moves, Move{from, to, NoPiece})
+
+			// Double move
+			if to = to + localN; rank == localFirstRank && p.board[to] == NoPiece {
+				moves = append(moves, Move{from, to, NoPiece})
+			}
+
+		}
+
+	}
+
+	// Take NE
+	if to := from + localN + localE; p.board[to] != NoPiece && p.board[to].Color() != myColour {
+		moves = append(moves, Move{from, to, NoPiece})
+	}
+	// Take NW
+	if to := from + localN + localW; p.board[to] != NoPiece && p.board[to].Color() != myColour {
+		moves = append(moves, Move{from, to, NoPiece})
+	}
+
 	return moves
 }
