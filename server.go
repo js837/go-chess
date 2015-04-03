c8407309ef8ba8b4f5827a2ca97a3833623ae6db
Jake <stacey.jake@gmail.com>
2015-04-03 18:17:37 +0100

    Imporved UI

--- server.go
+++ server.go
@@ -8,27 +8,24 @@ import (
 	"time"
 )
 
-func SetFen(w http.ResponseWriter, request *http.Request) {
+func RandomMove(w http.ResponseWriter, request *http.Request) {
 
 	defer request.Body.Close()
 	fen, _ := ioutil.ReadAll(request.Body)
-
 	p := FromFen(string(fen))
 
 	moves := p.GetMoves(p.turn)
 
 	rand.Seed(time.Now().Unix())
 	i := rand.Intn(len(moves))
-
 	newPosition := p.ApplyMove(moves[i])
 
 	fmt.Fprintf(w, PositionToBoardFen(&newPosition))
-
 }
 
 func main() {
 
-	http.HandleFunc("/setfen", SetFen)
+	http.HandleFunc("/random", RandomMove)
 
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.ListenAndServe(":8000", nil)
