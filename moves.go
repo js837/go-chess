150781166a86e1d8758dd61d5eba7a3170e90f97
Jake <stacey.jake@gmail.com>
2015-04-05 11:18:04 +0100

    Minimax works

--- moves.go
+++ moves.go
@@ -216,7 +216,7 @@ func PawnMoves(p *Position, from int) []Move {
 	// Promotion (only Queen for now)
 	var promotion Piece
 	if rank == localPromoRank {
-		promotion = Piece(Queen | p.turn)
+		promotion = Piece(Queen | myColour)
 	} else {
 		promotion = NoPiece
 	}
